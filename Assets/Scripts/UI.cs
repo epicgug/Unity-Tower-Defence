9035106ada511146710707051a3d5a89415dcc80
Justin Dachille <justindachille@gmail.com>
2017-01-29 18:48:34 -0800

    Added Wave Counter

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+	public Text waveText;
+	public GC GC;
+
+	// Use this for initialization
+	void Start () {
+		GC = GameObject.Find ("GameController").GetComponent<GC> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateText ();
+	}
+
+	void updateText() {
+		waveText.text = "Wave: " + (GC.currentWave + 1);
+	}
+}
